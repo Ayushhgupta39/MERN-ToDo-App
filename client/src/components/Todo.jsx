c7157965a81496730409137b83fcaaed7e3e208a
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-01-17 17:09:05 +0530

    Add Note functionality added

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,18 +1,30 @@
-import { Box, List, ListIcon, ListItem, Text } from '@chakra-ui/react'
-import React from 'react'
-import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
-
+import { Box, List, ListIcon, ListItem, Text } from "@chakra-ui/react";
+import React from "react";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 
 export default function Todo({ todo }) {
   return (
     <Box>
-        <List>
-            <ListItem>
-                { todo.data }
-                <ListIcon as={EditIcon}/>
-                <ListIcon as={DeleteIcon}/>
-            </ListItem>
-        </List>
+      <List>
+        <ListItem
+          display={"flex"}
+          justifyContent="space-between"
+          width={{
+            base: '72',
+            md: '72',
+            xl: '96'
+          }}
+          bgColor='cyan.50'
+          p={'2'}
+          m={'2'}
+        >
+          {todo.data}
+          <Box>
+          <ListIcon as={EditIcon} />
+          <ListIcon as={DeleteIcon} />
+          </Box>
+        </ListItem>
+      </List>
     </Box>
-  )
+  );
 }
